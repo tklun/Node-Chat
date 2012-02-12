35d45da9f06c3c2b2ed54b202e006a041f41e05a
Tom Klun <tom@tomklun.com>
2012-02-12 17:43:14 -0600

    Added socket js stuff

---
+++ public/javascripts/socket-circles.js
@@ -0,0 +1,21 @@
+var socketCircle = socketCircle || {};
+socketCircle.init = function() {
+  socketCanvas = document.getElementById('socket-canvas');
+  socketCanvas.setAttribute('width', window.innerWidth );
+  socketCanvas.setAttribute('height', window.innerHeight );
+  socketCtx = socketCanvas.getContext('2d');
+};
+
+socketCircle.Circles = [];
+socketCircle.Circles.i = 0;
+socketCircle.create = function(circle, context) {
+  var r = Math.floor(Math.random() * 256),
+      g = Math.floor(Math.random() * 256),
+      b = Math.floor(Math.random() * 256);
+
+    context.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + '.8' + ')';
+    context.beginPath();
+    context.arc(circle.mx, circle.my, socketCircle.Circles.i * 2, 0, Math.PI * 2, true);
+    context.fill();
+    socketCircle.Circles.i++;
+};
